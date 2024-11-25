12c910555db0704d716bc368a441e7e90c1faec8
marioldg <matuteldgmario@gmail.com>
2024-11-25 18:07:41 +0100

    ejercicio1

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,17 @@
-fun main() {
-    println("Hello World!")
-}
+    fun main() {
+
+        for (i:Int in 1..100){
+            if(i%3==0){
+                println("tri")
+
+            }else if(i%5==0){
+                println("qui")
+
+            }else if(i%3==0 && i%5==0){
+            println("triqui")
+
+            }else{
+                println(i)
+            }
+        }
+    }
