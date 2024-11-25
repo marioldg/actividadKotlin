c13328f36a36855e751b252e7cd60a2ada3b9141
marioldg <matuteldgmario@gmail.com>
2024-11-25 18:48:42 +0100

    ejercicio2Final

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,6 @@
     fun main() {
         println("Introduce la primera palabra ")
-        val palabra1 = readln() ?: " "
+        val palabra1 = readln()?: " "
         println("Introduce la segunda palabra")
         val palabra2 = readln()
 
