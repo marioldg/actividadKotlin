79fc4ef67b8c51d6fda78292e650ee2362fef1a9
marioldg <matuteldgmario@gmail.com>
2024-11-25 19:37:16 +0100

    ejercicio3SinAcabar

--- src/Main.kt
+++ src/Main.kt
@@ -1,17 +1,31 @@
     fun main() {
+        val operacion1= "{a + b [c] * (2x2)}}}}"
+        val operacion2="{ [ a * ( c + d ) ] - 5 }"
+        val operacion3=" { a * ( c + d ) ] - 5 }"
+        val operacion4="{a^4 + (((ax4)}"
+        val operacion5="{ ] a * ( c + d ) + ( 2 - 3 )[ - 5 }"
+        val operacion6="{{{{{{(}}}}}}"
+        val operacion7="(a"
+        estaBalanceado(operacion1)
 
-        for (i:Int in 1..100){
-            if(i%3==0){
-                println("tri")
-
-            }else if(i%5==0){
-                println("qui")
-
-            }else if(i%3==0 && i%5==0){
-            println("triqui")
-
-            }else{
-                println(i)
-            }
-        }
     }
+
+    fun estaBalanceado(Operacion:String):Boolean{
+        var simbolos:MutableList<Char> = mutableListOf()
+
+
+        for (i in Operacion){
+            if(i=='{' || i=='[' || i=='('){
+                simbolos.addFirst(i)
+            }else if (i=='}' || i==']' || i==')'){
+                simbolos.addLast(i)
+            }else{
+                simbolos.remove(i)
+            }
+
+        }
+        return false
+
+
+    }
+
