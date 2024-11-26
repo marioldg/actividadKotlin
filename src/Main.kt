906da5536beec9ac89b339ed23b5a60919b84871
marioldg <matuteldgmario@gmail.com>
2024-11-26 19:42:31 +0100

    ejercicio4

--- src/Main.kt
+++ src/Main.kt
@@ -1,17 +1,55 @@
     fun main() {
+        val acciones = arrayOf("saltar", "saltar", "saltar", "saltar", "saltar")
+        val pista = "||_||"
+        val resultado = Carrera(acciones, pista)
+        println("Carrera superada?: $resultado")
 
-        for (i:Int in 1..100){
-            if(i%3==0){
-                println("tri")
+        /**["correr", "saltar", "correr", "saltar", "correr"] y "_|_|_"
+        ["correr", "correr", "correr", "saltar", "correr"] y "_|_|_"
+        ["correr", "correr", "saltar", "saltar", "correr"] y "_|_|_"
+        ["correr", "correr", "saltar", "saltar", "correr"] y "_|_|_|_"
+        ["correr", "saltar", "correr", "saltar"] y "_|_|_"
+        ["correr", "saltar", "correr", "saltar", "correr", "saltar", "correr"] y "_|_|_"
+        ["saltar", "saltar", "saltar", "saltar", "saltar"] y "|||||"
+        ["saltar", "saltar", "saltar", "saltar", "saltar"] y "||_||"*/
 
-            }else if(i%5==0){
-                println("qui")
-
-            }else if(i%3==0 && i%5==0){
-            println("triqui")
-
-            }else{
-                println(i)
-            }
-        }
     }
+
+    fun Carrera(acciones: Array<String>, suelo: String): Boolean {
+        if (acciones.size != suelo.length) {
+            return false
+        } else {
+            var carreraSuperada = true
+            val sueloModificado = StringBuilder(suelo)
+
+            for (i in acciones.indices) {
+
+                if (acciones[i] != "correr" && acciones[i] != "saltar") {
+                    sueloModificado[i]='?'
+                    return false
+                }
+
+                when {
+                    acciones[i] == "correr" && suelo[i] == '_' -> {}
+                    acciones[i] == "saltar" && suelo[i] == '|' -> {}
+                    acciones[i] == "saltar" && suelo[i] == '_' -> {
+                        sueloModificado[i] = 'x'
+                        carreraSuperada = false
+                    }
+                    acciones[i] == "correr" && suelo[i] == '|' -> {
+                        sueloModificado[i] = '/'
+                        carreraSuperada = false
+                    }
+
+                    else -> {
+                        sueloModificado[i] = '?'
+                        carreraSuperada = false
+                    }
+                }
+            }
+
+            println("Resultado de la pista: ${sueloModificado.toString()}")
+            return carreraSuperada
+        }
+
+            }
